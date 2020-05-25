a945b73072e4a3beebb035531f50436a962303ee
Artymovych Maria <mashkaart94@gmail.com>
2020-05-25 17:46:52 +0300

    settings and profile modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'homepage', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) }, { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
