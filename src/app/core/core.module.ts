21699fc4168defd50385a86ac8bd2371d41120ba
Artymovych Maria <mashkaart94@gmail.com>
2020-05-27 03:00:53 +0300

    angular material toolbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule],
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent],
 })
